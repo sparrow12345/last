c5147322944e4a0469de9f9c105219b59f4c81cb
sparrow12345 <m.naser@innopolis.university>
2023-04-05 00:31:59 +0300

    Updates

---
+++ routes/+layout.svelte
@@ -0,0 +1,87 @@
+<script>
+	import './styles.css';
+	import { base } from '$app/paths';
+</script>
+
+<div class="app">
+	<header>
+		<h1>Majed Naser Personal Website</h1>
+		<nav>
+			<a href="{base}/">Portfolio</a>
+			<a href="{base}/about">About</a>
+			<a href="{base}/experience">Experience</a>
+			<a href="{base}/education">Education</a>
+			<a href="{base}/projects">Projects</a>
+			<a href="{base}/comic">Comic</a>
+		</nav>
+	</header>
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>Copyright &copy; 2023 by Software Developer</p>
+	</footer>
+</div>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	nav {
+		display: flex;
+		justify-content: center;
+		--background: rgba(255, 255, 255, 0.7);
+	}
+	nav a {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		padding: 0 0.5rem;
+		color: var(--color-text);
+		font-weight: 700;
+		font-size: 0.8rem;
+		text-transform: uppercase;
+		letter-spacing: 0.1em;
+		text-decoration: none;
+		transition: color 0.2s linear;
+	}
+
+	a:hover {
+		color: var(--color-theme-1);
+	}
+
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
---
+++ routes/about/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+	import github from '$lib/images/github.png';
+	import google from '$lib/images/google.png';
+	import Telegram from '$lib/images/telegram.png';
+</script>
+
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<h2>About Me</h2>
+<p>
+	Data Scientist, Software Developer, with a growing thirst for knowledge, and a never-ending
+	passion for IT.
+</p>
+
+<h2>Contacts</h2>
+<ul>
+	<li>
+		<img src={github} width="15" height="18" style="vertical-align:bottom" alt="Github" />
+		<a id="gh-link" href="https://github.com/sparrow12345"> GitHub </a>
+	</li>
+	<li>
+		<img src={Telegram} width="15" height="18" style="vertical-align:bottom" alt="Telegram" />
+		<a id="tg-link" href="https://t.me/Isildur_son_of_Elendil"> Telegram </a>
+	</li>
+	<li>
+		<img src={google} width="15" height="18" style="vertical-align:bottom" alt="Google" />
+		<a id="email" href="mailto:majdnaser2013@gmail.com"> Email </a>
+	</li>
+</ul>
